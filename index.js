a0366f9c82a241547f5d82305fa5e2705f797f09
Gabriel <gabriel.aug.alves@outlook.com>
2021-09-09 15:00:10 -0300

    Teste de logicas ejs

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Bem vindo ao meu site!");
+// Usando o express para dizer que o EJS é a engine
+app.set('view engine', 'ejs');
+
+// Rotas
+app.get("/:nome/:lang", (req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = true;
+
+    var produtos = [
+        {nome: "Doritos", preco: 3.14},
+        {nome: "Coca-cola", preco: 5.00},
+        {nome: "Leite", preco: 1.45}
+    ]
+
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        empresa: "DrogaBella",
+        inscritos: 8000,
+        msg: exibirMsg,
+        produtos: produtos
+    });
 });
 
+// Caminho
 app.listen(8080,()=>{console.log("App Rodando!")});
