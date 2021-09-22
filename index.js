b95424e5a78b1a6f789b6dd66416fcef6cbaf4a1
GabrielAugustinho <gabriel.aug.alves@outlook.com>
2021-09-22 16:47:46 -0300

    Zerando e importando bootstrap

--- index.js
+++ index.js
@@ -5,25 +5,8 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // Rotas
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
-
-    var produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca-cola", preco: 5.00},
-        {nome: "Leite", preco: 1.45}
-    ]
-
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "DrogaBella",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+app.get("/", (req, res) => {    
+    res.render("index");
 });
 
 // Caminho
